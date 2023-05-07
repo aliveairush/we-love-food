6738d64cc1bb87189ad56b8af3e3daabf083916b
Aimurzin Ainur <aliveairush@gmail.com>
2023-05-07 17:50:43 +0300

    Tsconfig for autoimport by vscode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'we-love-food';
+
+  constructor(private _http: HttpClient) {}
+
+  ngOnInit() {
+    this._http.get('http://localhost:3000/hello').subscribe((res: any) => {
+      this.title = res.answer;
+      console.log(res);
+    });
+  }
 }
